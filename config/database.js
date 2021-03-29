fcbabe43fb42c4fd72f76d1f1556112c92238f7a
niel63 <placmedia@gmail.com>
2021-03-29 02:39:55 +0100

    updated

--- config/database.js
+++ config/database.js
@@ -4,20 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'naijaNinja'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: env('DATABASE_URI'),
+        database: 'nairaavenue',
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
   },
 });
+
+
 
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
@@ -25,12 +22,18 @@ module.exports = ({ env }) => ({
 //     default: {
 //       connector: 'mongoose',
 //       settings: {
-//         uri: env('DATABASE_URI'),
-//         database: 'nairaavenue',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         srv: env.bool('DATABASE_SRV', false),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'naijaNinja'),
+//         username: env('DATABASE_USERNAME', null),
+//         password: env('DATABASE_PASSWORD', null),
 //       },
 //       options: {
-//         ssl: true,
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
 //       },
 //     },
 //   },
 // });
+
